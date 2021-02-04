b922052256e5f593544f75f12785978b63268ab5
Alejandro Sancho <34617@DESKTOP-QUD65H0>
2021-02-04 18:15:41 +0100

    Test GetDepositoNivelTest()

---
+++ DepositoCombustible/src/DepositoCombustibleTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class DepositoCombustibleTest {
+
+    static DepositoCombustible tank; 
+    
+    @BeforeAll
+    public static void before() {
+    	tank = new DepositoCombustible(40.0,0.0);
+    }
+	
+	@Test
+	public void getDepositoNivelTest() {
+		
+		double exp = 0.0;
+		double res = tank.getDepositoNivel();
+		assertEquals(exp, res, 0.1);
+		
+	}
+
+}
